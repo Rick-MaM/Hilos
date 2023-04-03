e9001ff6467b03194668eff6cd62d86baa37f75d
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-02 20:21:37 -0600

    Se crea la clase move_image

--- Prcatica_6_Hilos.py
+++ Prcatica_6_Hilos.py
@@ -2,3 +2,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 import threading
 import time
+
+
+class move_image:
+
+    def __init__(self, window, image, direction):
+        self.window = window
+        self.image = image
+        self.direction = direction
+        self.limit_date = False
