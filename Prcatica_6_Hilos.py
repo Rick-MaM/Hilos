bf398995fc9cd4326be39a5c88a1a03039cfa48d
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-02 20:28:56 -0600

    Se crea la funcion de limit

--- Prcatica_6_Hilos.py
+++ Prcatica_6_Hilos.py
@@ -11,7 +11,14 @@ class move_image:
         self.image = image
         self.direction = direction
         self.limit_date = False
+        self.insert()
 
     def insert(self):
         self.lbl_image = Label(self.window, image=self.image)
         self.move_image(0,0)
+    
+    def limit(self, date_x_or_y):
+        if date_x_or_y >= 400:
+            self.limit_date = True
+        elif date_x_or_y <= 0:
+            self.limit_date = False
