305a360fb9d1ce527dbe9da8102cae7c34ad47c3
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-04-02 20:30:37 -0600

    Se crea la funcion de move_image

--- Prcatica_6_Hilos.py
+++ Prcatica_6_Hilos.py
@@ -30,3 +30,12 @@ class move_image:
         else:
             date += 5
         return date
+    
+    def move_image(self, date_x, date_y):
+        while True:
+            if self.direction == "left_to_right":
+                date_x = self.increase_or_decrease(date_x)
+            elif self.direction == "up_to_bottom":
+                date_y = self.increase_or_decrease(date_y)
+            self.lbl_image.place(x=date_x, y=date_y)
+            time.sleep(0.15)
